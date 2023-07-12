be0fe31fcc7c52abb092f408451d3b022a7f6e70
kodsurfer <kodsurfer@ya.ru>
2023-07-12 11:32:03 +0300

    add base58Encode

--- shortener/shortener.go
+++ shortener/shortener.go
@@ -1,10 +1,26 @@
 package shortener
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"fmt"
+	"github.com/itchyny/base58-go"
+	"os"
+)
 
 func getSha256(s string) []byte {
 	algo := sha256.New()
 	algo.Write([]byte(s))
 
 	return algo.Sum(nil)
+}
+
+func base58Encode(bytes []byte) string {
+	encoding := base58.BitcoinEncoding
+	enc, err := encoding.Encode(bytes)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	return string(enc)
 }
